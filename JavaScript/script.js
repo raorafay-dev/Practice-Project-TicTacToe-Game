4f2bb6dcd81160294437fc9124b9199d40283c6c
Rana Rafay <73307548+ranarafay@users.noreply.github.com>
2022-08-11 01:10:35 +0500

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,85 @@
+console.log("By Rana Rafay");
+
+let audioTurn = new Audio("source files/ting.mp3");
+
+let turn = "X";
+let gameOver = false;
+
+// function to change the turn
+const changeTurn = () => {
+    return turn === "X" ? "O" : "X";
+}
+
+// function to check for a win
+const CheckWin = () => {
+    let boxText = document.getElementsByClassName("boxtext");
+
+    let wins = [
+        // horizontal
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+
+        // vertical
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+
+        // diagonal
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+
+    wins.forEach(e => {
+        if ((boxText[e[0]].innerText === boxText[e[1]].innerText) && (boxText[e[2]].innerText === boxText[e[1]].innerText) && (boxText[e[0]].innerText !== '')) {
+            document.querySelector('.info').innerText = boxText[e[0]].innerText + " Won"
+            gameOver = true;
+            document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width = "200px";
+            document.querySelector('.line').style.width = `20vw`;
+            document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+
+            let squares = document.getElementsByClassName("box");
+            squares[e[0]].classList.add("backgroundColor");
+            squares[e[1]].classList.add("backgroundColor");
+            squares[e[2]].classList.add("backgroundColor");
+        }
+    })
+}
+
+
+
+// game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxText = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxText.innerText === '') {
+            boxText.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            CheckWin();
+            if (!gameOver) {
+                document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
+            }
+        }
+    })
+})
+
+// reset button
+reset.addEventListener('click', () => {
+    let boxText = document.querySelectorAll('.boxtext');
+    Array.from(boxText).forEach(element => {
+        element.innerText = "";
+    });
+    turn = 'X';
+    gameOver = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
+    document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width = "0px";
+    document.querySelector('.line').style.width = `0vw`;
+    let squares = document.getElementsByClassName("box");
+    Array.from(squares).forEach(element => {
+        element.classList.remove("backgroundColor");
+    });
+
+
+})
